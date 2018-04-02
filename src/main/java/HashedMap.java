09080593367801472683941bdbd7ddc08435d47a
Richard Szombat <szombatrichard@gmail.com>
2018-04-02 23:45:57 +0200

    no resize

---
+++ src/main/java/HashedMap.java
@@ -0,0 +1,68 @@
+import java.util.LinkedList;
+
+class KeyValue<K, V> {
+
+    public K key;
+    public V value;
+}
+
+
+public class HashedMap<K, V> {
+
+    private int bucketSize = 16;
+
+    private LinkedList<KeyValue<K, V>>[] elements = new LinkedList[bucketSize];
+
+    public void add(K key, V value) {
+        int position = getHash(key);
+        KeyValue newKeyValue = new KeyValue();
+        newKeyValue.key = key;
+        newKeyValue.value = value;
+        if (elements[position] != null) {
+            for (KeyValue keyValue : elements[position]) {
+                if (keyValue.key.equals(key)) {
+                    throw new IllegalArgumentException();
+                } else {
+                    elements[position].add(newKeyValue);
+                    return;
+                }
+            }
+        } else {
+            LinkedList<KeyValue<K, V>> list = new LinkedList<>();
+            list.add(newKeyValue);
+            elements[position] = list;
+        }
+    }
+
+    public void remove (K key) {
+        int position = getHash(key);
+        if (elements[position] == null) {
+            throw new IllegalArgumentException();
+        }
+        for (KeyValue keyValue : elements[position]) {
+            if (keyValue.key.equals(key)) {
+                elements[position].remove(keyValue);
+                return;
+            }
+        }
+        throw new IllegalArgumentException();
+    }
+
+    public Object getValue(K key) {
+        int position = getHash(key);
+        for (KeyValue keyValue: elements[position]) {
+            if (keyValue.key.equals(key)) {
+                return keyValue.value;
+            }
+        }
+        throw new IllegalArgumentException();
+    }
+
+    private int getHash(K key) {
+        return key.hashCode() % bucketSize;
+    }
+
+    public void clearAll() {
+        elements = new LinkedList[bucketSize];
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,14 @@
+
 public class Main {
+
+    public static void main(String[] args) {
+        HashedMap hashedMap =  new HashedMap<>();
+        for (int i = 0; i <17; i++) {
+            hashedMap.add(i,i);
+        }
+        System.out.println(hashedMap.getValue(2));
+        hashedMap.remove(2);
+        System.out.println(3);
+
+    }
 }
